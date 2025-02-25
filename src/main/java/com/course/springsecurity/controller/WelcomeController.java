cec03edce0f3b1850d717fd4e8dc8c286dd49eb8
abhisheksingh <abhisheksingh.geek123@gmail.com>
2025-02-25 07:51:23 +0530

    Creating a Simple Spring Boot App without Security and adding logger in color patterns

---
+++ src/main/java/com/course/springsecurity/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.course.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String saWelcome(){
+        return "Welcome to Spring Application without Security";
+    }
+}
